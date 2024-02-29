ca99e6baa575f69442b58e1870e54d27db8fa67e
Nolan <nolanbeguhl@gmail.com>
2024-02-29 13:10:37 -0800

    Logic for game.

--- game.js
+++ game.js
@@ -6,7 +6,8 @@ function getComputerSelection() {
 
 function playRound(computerSelection, playerSelection) {
     let winner = "";
-    if(computerSelection === "rock" && playerSelection === "paper") {
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection === "rock" && playerSelection === "paper") {
         winner = "computer";
     } else if (computerSelection === "paper" && playerSelection === "rock") {
         winner = "computer";
@@ -20,4 +21,30 @@ function playRound(computerSelection, playerSelection) {
     }
 
     return winner;
+}
+
+function playGame() {
+
+    const overall = 0;
+
+    for (let i = 0; i <= 4; i++) {
+        let computerSelection = getComputerSelection()
+        do {
+            let playerSelection = prompt("Enter rock, paper, or scissors.", "").toLowerCase();;
+            if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+                break;
+            } else {
+                console.log("Invalid input. Enter one of the aforementioned strings.")
+            }
+        } while (true);
+
+        if (playRound(computerSelection, playerSelection) === "computer") {
+            console.log(`You lose because the computer's ${computerSelection} beats your ${playerSelection}!`)
+        } else if (playRound(computerSelection, playerSelection) === "player") {
+            console.log(`You win because the computer's ${computerSelection} loses to your ${playerSelection}!`)
+        } else {
+            console.log(`It's a draw because the computer's ${computerSelection} ties your ${playerSelection}!`)
+        }
+    }
+
 }
